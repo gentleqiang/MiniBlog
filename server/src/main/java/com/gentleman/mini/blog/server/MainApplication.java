0e2c3dfef56a631124a49f844d2c981517a47b81
gentleqiang <gentleman_qiang@163.com>
2021-04-13 22:46:28 +0800

    serverpom文件更新

--- server/src/main/java/com/gentleman/mini/blog/server/MainApplication.java
+++ server/src/main/java/com/gentleman/mini/blog/server/MainApplication.java
@@ -1,16 +1,21 @@
 package com.gentleman.mini.blog.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author 一粒尘埃
  * @date 2021/4/7/22:03
  */
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class,args);
